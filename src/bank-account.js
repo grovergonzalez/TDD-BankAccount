9f91ddca015894097e3a7e85893fd9cd152e4ebc
grovergonzalez <grover.gonzalez@ucb.edu.bo>
2023-03-22 22:08:16 -0400

    Superando los errores del test

--- src/bank-account.js
+++ src/bank-account.js
@@ -1,36 +1,31 @@
-//
-// This is only a SKELETON file for the 'Bank Account' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
 
 export class BankAccount {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+
+  constructor(monto) {
+    this.activo = false;
+    this.monto = monto;
   }
 
   open() {
-    throw new Error("Remove this statement and implement this function");
+    if (this.activo) throw new ValueError();
+    this.activo = true;
+    this.monto = 0;
   }
 
   close() {
-    throw new Error("Remove this statement and implement this function");
+    //throw new Error("Remove this statement and implement this function");
   }
 
   deposit() {
-    throw new Error("Remove this statement and implement this function");
+   // throw new Error("Remove this statement and implement this function");
   }
 
   withdraw() {
-    throw new Error("Remove this statement and implement this function");
+   // throw new Error("Remove this statement and implement this function");
   }
 
   get balance() {
-    throw new Error("Remove this statement and implement this function");
-  }
-}
-
-export class ValueError extends Error {
-  constructor() {
-    super("Bank account error");
+   // throw new Error("Remove this statement and implement this function");
+    return this.monto;
   }
 }
